14aa1b0c6d657b49a684da2cb9b537a39b5e92b6
Your Name <you@example.com>
2025-06-26 18:06:54 -0400

    final incluido en gestor_series.c

---
+++ TDAs/gestor_series.c
@@ -0,0 +1,129 @@
+void gestor_LiberarMemoriaMap(GestorSeries *gestor)
+{
+    if (!gestor)
+    {
+        return;
+    }
+    MapPair *pair;
+
+    while ((pair = map_first(gestor->mapa)) != NULL)
+    {
+        map_remove(gestor->mapa, pair->key);                
+        gestor_LiberarYEliminarSerie((TipoSerie*)pair->value);
+        free(pair->key);                                    
+        free(pair);                                         
+    }
+    
+    free(gestor->mapa->buckets);
+    free(gestor->mapa);
+    
+    list_clean(gestor->favoritas);
+    free(gestor->favoritas);
+}
+
+int gestor_agregar(GestorSeries *gestor, TipoSerie *serie)
+{
+    if (!gestor || !serie)
+    {
+        return 0;
+    }
+    if (map_search(gestor->mapa, serie->titulo))
+    {
+        return 0;
+    }
+    map_insert(gestor->mapa, strdup(serie->titulo), serie);
+    return 1;
+}
+
+
+TipoSerie* gestor_buscarSerie(const GestorSeries *gestor, const char *titulo)
+{
+    if (!gestor || !titulo)
+    {
+        return NULL;
+    }
+    MapPair *pair = map_search(gestor->mapa, (char*)titulo);
+    return pair ? (TipoSerie*)pair->value : NULL;
+}
+
+
+void gestor_MostrarSeries(const GestorSeries *gestor)
+{
+    if (!gestor)
+    {
+        return;
+    }
+    for (MapPair *pair = map_first(gestor->mapa);
+         pair;
+         pair = map_next(gestor->mapa))
+    {
+        gestor_imprimirSerie((TipoSerie*)pair->value);
+    }
+}
+
+
+int gestor_eliminarYliberar(GestorSeries *gestor, const char *titulo)
+{
+    if (!gestor || !titulo)
+    {
+        return 0;
+    }
+    MapPair *pair = map_search(gestor->mapa, (char*)titulo);
+    if (!pair)
+    {
+        return 0;
+    }
+
+    char *clave = pair->key;
+    TipoSerie *serie = (TipoSerie*)pair->value;
+
+    map_remove(gestor->mapa, clave);
+
+    gestor_LiberarYEliminarSerie(serie);
+    free(clave);
+
+    return 1;
+}
+
+int gestor_agregarFavorito(GestorSeries *gestor, const char *titulo)
+{
+    if (!gestor || !titulo)
+    {
+        return 0;
+    }
+    TipoSerie *serie = gestor_buscarSerie(gestor, titulo);
+    if (!serie)
+    {
+        return 0;
+    }
+    for (TipoSerie *serieFav = list_first(gestor->favoritas);
+         serieFav;
+         serieFav = list_next(gestor->favoritas))
+    {
+        if (strcmp(serieFav->titulo, titulo) == 0)
+        {
+            return 0;
+        }
+    }
+    list_pushBack(gestor->favoritas, serie);
+    return 1;
+}
+
+void gestor_mostrarFavoritos(const GestorSeries *gestor)
+{
+    if (!gestor)
+    {
+        return;
+    }
+    if (list_size(gestor->favoritas) == 0)
+    {
+        puts("No hay series favoritas marcadas.\n");
+        return;
+    }
+    for (TipoSerie *serieFav = list_first(gestor->favoritas);
+         serieFav;
+         serieFav = list_next(gestor->favoritas))
+    {
+        gestor_imprimirSerie(serieFav);
+    }
+}
