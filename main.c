f29740a781c0c9fcbcd0d8e1c02dfc9c5661204e
Your Name <you@example.com>
2025-06-27 13:23:07 -0400

    Fusion archivos

--- main.c
+++ main.c
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 1d099d51dcd1fed3c57fd370c71fa64511ae8104
 int main (){
     printf("hola soy jm");
     //danilo estuvo aqui
     //danilo estuvo aqui por segunda vez
 }
-<<<<<<< HEAD
-// Me cai del quinto piso
-=======
->>>>>>> 1d099d51dcd1fed3c57fd370c71fa64511ae8104
